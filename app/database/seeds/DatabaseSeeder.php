4c2944a0dd427646db8e231d1c61decbff4de341
Justin Dietert <justin.dietert@gmail.com>
2015-05-11 13:31:38 -0500

    added Views, removed unneeded database files

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -10,15 +10,13 @@ class DatabaseSeeder extends Seeder {
 	 */
 	public function run()
 	{
-		// delete pre-existing versions of tables in the opposite order they 
+		// delete pre-existing versions of tables in the opposite order they
 		// were seeded in to avoid foreign key conflict errors
-		DB::table('posts')->delete();
 		DB::table('users')->delete();
 
 		Eloquent::unguard();
 
 		$this->call('UsersTableSeeder');
-		$this->call('PostsTableSeeder');
 	}
 
 }
