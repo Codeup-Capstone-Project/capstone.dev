568dc3b292f11a5074f54cf837c143552e3bad94
Jamie Haskell <jmehaskell@gmail.com>
2015-05-13 19:55:45 -0500

    edited a name attributes to match justins to avoid potential conflict

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -11,18 +11,18 @@ class UsersTableSeeder extends Seeder {
 
 		for($i = 0; $i < 100; $i++) {
 			$user = User::create(array(
-				'first'		=> $faker->firstName($gender = null|'male'|'female'),
-				'last'		=> $faker->lastname,
-				'username' 	=> $faker->unique()->userName,
-				'email' 	=> $faker->unique()->email,
-				'password' 	=> $faker->password
+				'first_name' => $faker->firstName($gender = null|'male'|'female'),
+				'last_name'	 => $faker->lastname,
+				'username' 	 => $faker->unique()->userName,
+				'email' 	 => $faker->unique()->email,
+				'password' 	 => $faker->password
 			));
 		}
 
 		// a user with known password for testing purposes
 		$user1 = new User;
-		$user1->first = 'John';
-		$user1->last = 'Doe';
+		$user1->first_name = 'John';
+		$user1->last_name = 'Doe';
 		$user1->username = 'guest';
 		$user1->email = 'guest@gmail.com';
 		$user1->password = $_ENV['USER_PASS'];
