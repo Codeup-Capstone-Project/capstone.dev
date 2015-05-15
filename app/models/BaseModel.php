3079e657530a5977f06fbc5d2369197fc8c50345
Justin Dietert <justin.dietert@gmail.com>
2015-05-15 12:11:15 -0500

    added mutator for created_at attribute

--- app/models/BaseModel.php
+++ app/models/BaseModel.php
@@ -9,12 +9,12 @@
 
 		// Accessor that automatically formats dates for all posts created_at date
 		// displays '24 minutes ago, 2 days ago, etc.'
-		// public function getCreatedAtAttribute($value)
-		// {
-		//     $utc = Carbon::createFromFormat($this->getDateFormat(), $value);
-		//     return $utc->setTimezone('America/Chicago')->diffForHumans();
-		// }
-	
+		public function getCreatedAtAttribute($value)
+		{
+		    $utc = Carbon::createFromFormat($this->getDateFormat(), $value);
+		    return $utc->setTimezone('America/Chicago')->subDay()->format('F j, Y');
+		}
+
 		// Accessor that automatically formats dates for all posts updated_at date
 		// displays '24 minutes ago, 2 days ago, etc.'
 		public function getUpdatedAtAttribute($value)
