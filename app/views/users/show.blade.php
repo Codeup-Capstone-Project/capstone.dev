c9f96c3de5399e6032acece7df38d7f4c652fa7a
Justin Dietert <justin.dietert@gmail.com>
2015-05-15 15:57:09 -0500

    update to profile page

--- app/views/users/show.blade.php
+++ app/views/users/show.blade.php
@@ -5,19 +5,19 @@
 @stop
 
 @section('content')
-    {{-- <section id="profile"> --}}
         <div class="pad-bot">
             <div class="row">
                 <div class="col s12 m8 offset-m2 l6 offset-l3">
                     <ul class="collection">
-                        <li class="collection-item"><h5 class="blue-grey-text text-darken-2">User Profile</h5></li>
+                        <li class="collection-item"><h4 class="blue-grey-text text-darken-2">User Profile</h4></li>
                         <li class="collection-item avatar">
                             <img src="/img/justin.jpg" alt="" class="circle">
                             <span class="title">{{{ Auth::user()->username }}}</span>
+                            <p>{{{ Auth::user()->email }}}</p>
                             <p>Joined {{{ Auth::user()->created_at }}}</p>
-                            <p>Second Line</p>
                             <a href="#" class="secondary-content"><i class="mdi-editor-mode-edit"></i> Edit</a>
                         </li>
+                        <li class="collection-item"><h5 class="blue-grey-text text-darken-2">Game Stats</h5></li>
                         <li class="collection-item">Alvin</li>
                         <li class="collection-item">Alvin</li>
                         <li class="collection-item">Alvin</li>
@@ -25,5 +25,4 @@
                 </div>
             </div>
         </div>
-    {{-- </section> --}}
 @stop
