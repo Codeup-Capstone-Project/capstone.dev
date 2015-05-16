0b862ce2e5a99571f65274d7272b7bf55bfda9d8
Jamie Haskell <jmehaskell@gmail.com>
2015-05-16 18:50:57 -0500

    started the leader board, currently loads top 5 scores by best game_time

---
+++ app/views/leader.blade.php
@@ -0,0 +1,22 @@
+<h1>This is the Leader Board</h1>
+
+	{{-- Pagination Links --}}
+	<div>
+		{{-- {{ $stats->links() }} --}}
+	</div>
+
+	@foreach($stats as $key => $stat)
+		<div class='row'>
+			<div class='col s12 m10'>
+				<h3>{{{ $key + 1 }}}: {{{ $stat->user->username }}}</h3>
+				<p>Finished in: {{{ $stat->game_time }}} </p>
+				<p>With: {{{ $stat->moves }}} moves</p>
+				<p>{{{ $stat->updated_at }}}</p>
+			</div>
+		</div>
+	@endforeach
+
+	{{-- Pagination Links --}}
+	<div>
+		{{-- {{ $stats->links() }} --}}
+	</div>
