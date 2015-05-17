3dfb98b9ffcad6c81fed405853bc688222c03aad
Jamie Haskell <jmehaskell@gmail.com>
2015-05-17 13:42:42 -0500

    created easy version of 3x3 for demo

--- public/js/game.js
+++ public/js/game.js
@@ -52,6 +52,15 @@ $.ajaxSetup({
 	    	// Easy Level Button for Demo-day
 	    	$("#easy").on('click', function(){
 	    		$(".blocks").remove();
+	    		$(".blocks").remove();
+	    		puzzleSize = $(this).data('value');
+	    		setBlockDimensions();
+	    		totalBlocks = puzzleSize * puzzleSize;
+	    		cells = [];
+	    		initialBlockPositions = [1, 2, 3, 4, 0, 5, 7, 8, 6];
+	    		postInitialData();
+	    		$(".level").addClass('hidden');
+				$("#start, #cancel, .ready").removeClass('hidden');
 	    	});
 
 
