139bbe4f87827f087a81fc4b03070331affa5ebb
Justin Dietert <justin.dietert@gmail.com>
2015-05-18 13:03:06 -0500

    added some minor info

--- app/views/partials/footer.blade.php
+++ app/views/partials/footer.blade.php
@@ -2,32 +2,32 @@
             <div class="container">
                 <div class="row">
                     <div class="col l6 s12">
-                        <h5 class="blue-grey-text text-darken-1">Company Bio</h5>
+                        <h5 class="blue-grey-text text-darken-1">TyleNinja</h5>
                         <p class="blue-grey-text text-darken-1">We are a team of Codeup students working on this project like it's our full time job. If you would like to donate any amount so we may continue development, please contact us.</p>
                     </div>
                     <div class="col l3 s12">
                         <h5 class="blue-grey-text text-darken-1">Settings</h5>
                         <ul>
-                            <li><a class="blue-grey-text text-darken-1" href="#!">Link 1</a></li>
-                            <li><a class="blue-grey-text text-darken-1" href="#!">Link 2</a></li>
-                            <li><a class="blue-grey-text text-darken-1" href="#!">Link 3</a></li>
-                            <li><a class="blue-grey-text text-darken-1" href="#!">Link 4</a></li>
+                            <li><a class="blue-grey-text text-darken-1" href="#">Link 1</a></li>
+                            <li><a class="blue-grey-text text-darken-1" href="#">Link 2</a></li>
+                            <li><a class="blue-grey-text text-darken-1" href="#">Link 3</a></li>
+                            <li><a class="blue-grey-text text-darken-1" href="#">Link 4</a></li>
                         </ul>
                     </div>
                     <div class="col l3 s12">
                         <h5 class="blue-grey-text text-darken-1">Connect</h5>
                         <ul>
-                            <li><a class="blue-grey-text text-darken-1" href="#!">Link 1</a></li>
-                            <li><a class="blue-grey-text text-darken-1" href="#!">Link 2</a></li>
-                            <li><a class="blue-grey-text text-darken-1" href="#!">Link 3</a></li>
-                            <li><a class="blue-grey-text text-darken-1" href="#!">Link 4</a></li>
+                            <li><a class="blue-grey-text text-darken-1" href="#">GitHub</a></li>
+                            <li><a class="blue-grey-text text-darken-1" href="#">Link 2</a></li>
+                            <li><a class="blue-grey-text text-darken-1" href="#">Link 3</a></li>
+                            <li><a class="blue-grey-text text-darken-1" href="#">Link 4</a></li>
                         </ul>
                     </div>
                 </div>
             </div>
             <div class="footer-copyright">
                 <div class="container">
-                    Copyright &copy; {{ date('Y') }} J-squared.
+                    Copyright &copy; {{ date('Y') }} TyleNinja.
                 </div>
             </div>
         </footer>
