9ed5a53fa0cf1a821a035c13e9ae78e8a7d25ae2
Jamie Haskell <jmehaskell@gmail.com>
2015-05-17 20:17:16 -0500

    added 3 AJAX get requests for the leaderboard modal

--- public/js/init.js
+++ public/js/init.js
@@ -11,7 +11,28 @@
         },"slow")
     });
 
-    $('.modal-trigger').leanModal();
+    $('.modal-trigger#size3x3').leanModal({ // Callback for Modal open
+        ready: function() {
+            $.get('/play/leaders/3', null, function(data){
+                $("#leaderModalContent").html(data);
+            });
+        } 
+    });
 
+    $('.modal-trigger#size4x4').leanModal({ // Callback for Modal open
+        ready: function() {
+            $.get('/play/leaders/4', null, function(data){
+                $("#leaderModalContent").html(data);
+            });
+        } 
+    });
+
+    $('.modal-trigger#size5x5').leanModal({ // Callback for Modal open
+        ready: function() {
+            $.get('/play/leaders/5', null, function(data){
+                $("#leaderModalContent").html(data);
+            });
+        } 
+    });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
