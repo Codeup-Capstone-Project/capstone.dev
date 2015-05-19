add8d0f9ffb70286966fc787881ac7d8a19808c2
Jamie Haskell <jmehaskell@gmail.com>
2015-05-19 15:18:35 -0500

    linkedin and facebook TEST routes are both working

--- app/routes.php
+++ app/routes.php
@@ -12,12 +12,14 @@
 */
 
 Route::get('/', 'HomeController@showHome');
-
-Route::controller('users', 'UsersController');
-
 Route::get('login', 'HomeController@login');
 Route::post('login', 'HomeController@doLogin');
 Route::get('logout', 'HomeController@logout');
+
+Route::get('loginfb', 'SocialAuthController@loginWithFacebook');
+Route::get('loginli', 'SocialAuthController@loginWithLinkedin');
+
+Route::controller('users', 'UsersController');
 
 Route::controller('play', 'GameController');
 
@@ -35,15 +37,109 @@ Route::get('in', function() {
             // This was a callback request from linkedin, get the token
             $token = $linkedinService->requestAccessToken( $code );
             // Send a request with it. Please note that XML is the default format.
-            $result = json_decode($linkedinService->request('/people/~?format=json'), true);
+            $result = json_decode($linkedinService->request('/people/~:(id,first-name,last-name,headline,member-url-resources,picture-urls::(original),location,public-profile-url,email-address)?format=json'), true);
 
             // Show some of the resultant data
-            echo 'Your linkedin first name is ' . $result['firstName'] . ' and your last name is ' . $result['lastName'] . "<br/>";
-
+            // echo 'Your linkedin first name is ' . $result['firstName'] . 
+            //     ' and your last name is ' . $result['lastName'] . 
+            //     ' and your email is ' . $result['emailAddress'] .
+            //     ' and your profile photo url is ' . $result['pictureUrls']['values'][0] . 
+            //     ' and your public profile url is ' . $result['publicProfileUrl'] . "<br/>";
 
             //Var_dump
             //display whole array().
-            dd($result);
+            // var_dump($result);
+
+
+            // get data from input
+            $user = [
+                    'linkedin_id'        => $result['id'],
+                    'email'              => $result['emailAddress'],
+                    'first_name'         => $result['firstName'],
+                    'last_name'          => $result['lastName'],
+                    'profile_photo_url'  => $result['pictureUrls']['values'][0],
+                    'profile_url'        => $result['publicProfileUrl']
+            ];
+
+            // for testing:
+            // $message = 'Your unique facebook user id is: ' . $user['facebook_id'] . PHP_EOL . 
+            //          ' your email is ' . $user['email'] . PHP_EOL .
+            //          ' your name is ' . $user['first_name'] . ' ' . $user['last_name'] . 
+            //          ' and the url to your profile photo is ' . $user['profile_photo_url'];
+            // echo $message. "<br/>";
+
+            // try to login
+            // get user from db by facebook_id
+            $userExists = User::where( [ 'linkedin_id' => $user['linkedin_id'] ] )->first();
+            // var_dump($userExists);
+
+            // check if user exists
+            if ( $userExists ) {
+                    // login user
+                    Auth::login( $userExists );
+
+                    // build message with some of the resultant data
+                    // $message = 'Your unique facebook user id is: ' . $user['facebook_id'] . ' and your name is ' . $user['first_name'];
+
+                    // redirect to user profile
+                    return Redirect::action( 'GameController@getIndex' );
+                            // ->with( 'flash_success', $message );
+
+            } else {
+                    // FIRST TIME LinkedIn LOGIN
+
+                    // create a unique username for them
+                    // only allow first 12 letters of last name
+                    $lastnameArray = str_split($user['last_name']);
+                    $lastnameArray = array_slice($lastnameArray, 0, 12);
+                    $lastname = implode($lastnameArray);
+                    // get the first letter of first name
+                    $firstnameArray = str_split($user['first_name']);
+                    $firstLetter = array_shift($firstnameArray);
+                    // concatinate first letter of first name to their last name
+                    $username = $firstLetter . $lastname;
+
+                    // check if it already exists in database
+                    $usernameExists = User::where([ 'username' => $username ])->first();
+
+                    // if it does, append an incrementing number to the back and keep checking 
+                    if ($usernameExists) {
+                        $suffix = 2;
+                        do {
+                            $username = $username . $suffix;
+                            $usernameExists = User::where([ 'username' => $username ])->first();
+                            $suffix++;
+                        } while($usernameExists);
+                    }
+
+                    // create new user and save it into db
+                    $newUser = new User;
+                    $newUser->first_name           = strtolower($user['first_name']);
+                    $newUser->last_name            = strtolower($user['last_name']);
+                    $newUser->username             = strtolower($username);
+                    $newUser->password             = $_ENV['USER_PASS'];
+                    $newUser->email                = $user['email'];
+                    $newUser->linkedin_id          = $user['linkedin_id'];
+                    $newUser->profile_photo_url    = $user['profile_photo_url'];
+                    $newUser->linkedin_profile_url = $user['profile_url'];
+                    $newUser->save();
+
+                    // login user
+                    Auth::login( $newUser );
+
+                    // build message with some of the resultant data
+                    // $message_success = 'Your unique facebook user id is: ' . $result['id'] . ' and your name is ' . $result['name'];
+                    // $message_notice = 'Account Created.';
+
+                    // redirect to game page
+                    return Redirect::action( 'GameController@getIndex' );
+                            // ->with( 'flash_success', $message_success )
+                            // ->with( 'flash_notice', $message_notice );
+
+            }
+
+
+
 
         }// if not ask for permission first
         else {
@@ -95,7 +191,8 @@ Route::get('fb', function() {
 	                'email'		  		=> $result['email'],
 	                'first_name'  		=> $result['first_name'],
 	                'last_name'	  		=> $result['last_name'],
-	                'profile_photo_url' => $picture['data']['url']
+	                'profile_photo_url' => $picture['data']['url'],
+                    'profile_url'       => $result['link']
 	        ];
 
 	        // for testing:
@@ -116,7 +213,7 @@ Route::get('fb', function() {
                     Auth::login( $userExists );
 
                     // build message with some of the resultant data
-                    $message = 'Your unique facebook user id is: ' . $user['facebook_id'] . ' and your name is ' . $user['first_name'];
+                    // $message = 'Your unique facebook user id is: ' . $user['facebook_id'] . ' and your name is ' . $user['first_name'];
 
                     // redirect to user profile
                     return Redirect::action( 'GameController@getIndex' );
@@ -126,10 +223,15 @@ Route::get('fb', function() {
                     // FIRST TIME FB LOGIN
 
             		// create a unique username for them
-            		// concatinate first letter of first name to their last name
-            		$firstnameArray = str_split($user['first_name']);
-            		$firstLetter = array_shift($firstnameArray);
-            		$username = $firstLetter . $user['last_name'];
+                    // only allow first 12 letters of last name
+                    $lastnameArray = str_split($user['last_name']);
+                    $lastnameArray = array_slice($lastnameArray, 0, 12);
+                    $lastname = implode($lastnameArray);
+                    // get the first letter of first name
+                    $firstnameArray = str_split($user['first_name']);
+                    $firstLetter = array_shift($firstnameArray);
+                    // concatinate first letter of first name to their last name
+                    $username = $firstLetter . $lastname;
 
             		// check if it already exists in database
             		$usernameExists = User::where([ 'username' => $username ])->first();
@@ -146,23 +248,24 @@ Route::get('fb', function() {
 
                     // create new user and save it into db
                     $newUser = new User;
-                    $newUser->first_name  = strtolower($user['first_name']);
-                    $newUser->last_name   = strtolower($user['last_name']);
-                    $newUser->username 	  = strtolower($username);
-                    $newUser->password    = $_ENV['USER_PASS'];
-                    $newUser->email 	  = $user['email'];
-                    $newUser->facebook_id = $user['facebook_id'];
-                    $newUser->profile_photo_url = $user['profile_photo_url'];
+                    $newUser->first_name           = strtolower($user['first_name']);
+                    $newUser->last_name            = strtolower($user['last_name']);
+                    $newUser->username 	           = strtolower($username);
+                    $newUser->password             = $_ENV['USER_PASS'];
+                    $newUser->email 	           = $user['email'];
+                    $newUser->facebook_id          = $user['facebook_id'];
+                    $newUser->profile_photo_url    = $user['profile_photo_url'];
+                    $newUser->facebook_profile_url = $user['profile_url'];
                     $newUser->save();
 
                     // login user
                     Auth::login( $newUser );
 
                     // build message with some of the resultant data
-                    $message_success = 'Your unique facebook user id is: ' . $result['id'] . ' and your name is ' . $result['name'];
-                    $message_notice = 'Account Created.';
+                    // $message_success = 'Your unique facebook user id is: ' . $result['id'] . ' and your name is ' . $result['name'];
+                    // $message_notice = 'Account Created.';
 
-                    // redirect to home page
+                    // redirect to game page
                     return Redirect::action( 'GameController@getIndex' );
                             // ->with( 'flash_success', $message_success )
                             // ->with( 'flash_notice', $message_notice );
