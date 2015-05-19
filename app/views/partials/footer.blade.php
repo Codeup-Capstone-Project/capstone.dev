391353a7e3947a4ae4c8e8491e240fcb477589ef
Justin Dietert <justin.dietert@gmail.com>
2015-05-19 16:36:45 -0500

    updated footer content

--- app/views/partials/footer.blade.php
+++ app/views/partials/footer.blade.php
@@ -1,26 +1,22 @@
         <footer class="page-footer blue-grey darken-4">
             <div class="container">
                 <div class="row">
-                    <div class="col l6 s12">
+                    <div class="col l5 s12">
                         <h5 class="blue-grey-text text-darken-1">TyleNinja</h5>
                         <p class="blue-grey-text text-darken-1">We are a team of Codeup students working on this project like it's our full time job. If you would like to donate any amount so we may continue development, please contact us.</p>
                     </div>
-                    <div class="col l3 s12">
-                        <h5 class="blue-grey-text text-darken-1">Settings</h5>
+                    <div class="col l3 offset-l1 s12">
+                        <h5 class="blue-grey-text text-darken-1">About</h5>
                         <ul>
-                            <li><a class="blue-grey-text text-darken-1" href="#">Link 1</a></li>
-                            <li><a class="blue-grey-text text-darken-1" href="#">Link 2</a></li>
-                            <li><a class="blue-grey-text text-darken-1" href="#">Link 3</a></li>
-                            <li><a class="blue-grey-text text-darken-1" href="#">Link 4</a></li>
+                            <li><a class="blue-grey-text text-darken-1" href="/#about">About the Game</a></li>
+                            <li><a class="blue-grey-text text-darken-1" href="/#creators">The Creators</a></li>
                         </ul>
                     </div>
                     <div class="col l3 s12">
                         <h5 class="blue-grey-text text-darken-1">Connect</h5>
                         <ul>
                             <li><a class="blue-grey-text text-darken-1" href="#">GitHub</a></li>
-                            <li><a class="blue-grey-text text-darken-1" href="#">Link 2</a></li>
-                            <li><a class="blue-grey-text text-darken-1" href="#">Link 3</a></li>
-                            <li><a class="blue-grey-text text-darken-1" href="#">Link 4</a></li>
+                            <li><a class="blue-grey-text text-darken-1" href="#">Email us</a></li>
                         </ul>
                     </div>
                 </div>
