700135e965406a7d084d2e1020de0f5bc6bb1185
Justin Dietert <justin.dietert@gmail.com>
2015-05-20 18:18:15 -0500

    working on soft deleting users

--- app/models/User.php
+++ app/models/User.php
@@ -4,10 +4,15 @@ use Illuminate\Auth\UserTrait;
 use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableTrait;
 use Illuminate\Auth\Reminders\RemindableInterface;
+use Illuminate\Database\Eloquent\SoftDeletingTrait;
 
 class User extends BaseModel implements UserInterface, RemindableInterface {
 
 	use UserTrait, RemindableTrait;
+
+	// Soft Delete
+    use SoftDeletingTrait;
+    protected $dates = ['deleted_at'];
 
 	/**
 	 * The database table used by the model.
