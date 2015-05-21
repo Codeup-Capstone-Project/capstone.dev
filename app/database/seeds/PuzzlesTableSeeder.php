ff3e7e1d1d9d15878aff7ed06612a5637ee4c09a
Jamie Haskell <jmehaskell@gmail.com>
2015-05-20 20:59:25 -0500

    took out unecessary serializers

--- app/database/seeds/PuzzlesTableSeeder.php
+++ app/database/seeds/PuzzlesTableSeeder.php
@@ -5,12 +5,12 @@ class PuzzlesTableSeeder extends Seeder {
 
 	public function run()
 	{
-		$array1 = [0, 1, 5, 3, 6, 7, 8, 2, 4];
+		$array1 = [1, 2, 3, 4, 0, 5, 7, 8, 6];
 		//standard puzzle options
 		$puzzle1 = new Puzzle;
 		$puzzle1->type = '3x3';
 		$puzzle1->size = 3;
-		$puzzle1->initial_block_positions = serialize($array1);
+		$puzzle1->initial_block_positions = $array1;
 		$puzzle1->save();
 
 		$array2 = [14, 3, 1, 10, 13, 5, 11, 0, 6, 12, 7, 8, 2, 4, 9, 15];
@@ -18,7 +18,7 @@ class PuzzlesTableSeeder extends Seeder {
 		$puzzle2 = new Puzzle;
 		$puzzle2->type = '4x4';
 		$puzzle2->size = 4;
-		$puzzle2->initial_block_positions = serialize($array2);
+		$puzzle2->initial_block_positions = $array2;
 		$puzzle2->save();
 
 		$array3 = [14, 2, 1, 10, 13, 5, 11, 3, 6, 12, 7, 8, 0, 4, 9, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
@@ -26,7 +26,7 @@ class PuzzlesTableSeeder extends Seeder {
 		$puzzle3 = new Puzzle;
 		$puzzle3->type = '4x4';
 		$puzzle3->size = 4;
-		$puzzle3->initial_block_positions = serialize($array3);
+		$puzzle3->initial_block_positions = $array3;
 		$puzzle3->save();
 	}
 
