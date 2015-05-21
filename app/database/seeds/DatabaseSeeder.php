640016ccc7d3ccd4ff3b1fa3a7544f32cae348fc
Jamie Haskell <jmehaskell@gmail.com>
2015-05-21 10:58:35 -0500

    added a Stats table seeder

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -11,13 +11,16 @@ class DatabaseSeeder extends Seeder {
 	public function run()
 	{
 		// delete pre-existing versions of tables in the opposite order they
-		// were seeded in to avoid foreign key conflict errors
+		// were seeded in, to avoid foreign key conflict errors
+		DB::table('stats')->delete();
+		DB::table('puzzles')->delete();
 		DB::table('users')->delete();
 
 		Eloquent::unguard();
 
 		$this->call('UsersTableSeeder');
 		$this->call('PuzzlesTableSeeder');
+		$this->call('StatsTableSeeder');
 	}
 
 }
